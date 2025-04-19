a47835d76089a6b5beeb0b4ce03adc8bb84e3e4d
Arien Alabar <133151513+taetark@users.noreply.github.com>
2025-04-18 21:43:57 -0600

    Add files via upload
    UNFINISHED

---
+++ smiski_list.py
@@ -0,0 +1,74 @@
+"""
+Smiski List
+By: @Arien Alabar
+"""
+def series_1():
+    #series 1 list
+    series_1 = ["Hugging Knees", "Sitting", "Looking Back", "Lounging", "Peeking", "Hiding", " The Scream", "Rudolph"]
+    return series_1 
+
+def series_2():
+    #series 2 list
+    series_2 = ["Kneeling", "Climbing", "Daydream", "Pushing", "Peeking", "Listening", "Birth of Venus","Snowman"]
+    return series_2
+
+def series_3():
+    series_3 = ["Bridge", "Climbing", "Handstand", "Peeking", "Hiding", "Little","The Thinker"]
+    return series_3
+
+def series_4():
+    series_4 = ["Sneaking", "Scared", "Relaxing", "Lazy", "Stuck", "Defeated", "Twin Angels"]
+    return series_4
+
+def toilet_series():
+    toilet_series = ["Peek-a-boo", "Little (Smelly)", "Squatting", "Helping out", "Resting", "Holding in", "Water Fountain"]
+    return toilet_series
+
+def bathroom_series():
+    bathroom_series = ["Shampooing", "Not Looking", "Scrubbing", "With Duck", "Dazed","Looking", "Water Droplet"]
+    return bathroom_series
+
+def living_series():
+    living_series = ["Daydreaming", "Playing", "Hiding", "Naptime","Thinking","Lifting", "Sunflower"]
+    return living_series
+
+def bed_series():
+    bed_series = ["Before rest", "Sleepy", "Co-sleeping", "Reading","At sleep","Fussing", "Crescent Moon"]
+    return bed_series
+
+def yoga_series():
+    yoga_series = ["Lotus Pose", "Twist Pose", "Shouldstand Pose", "Triangle Pose","Tree Pose", "Ship Pose","Twin Hearts"]
+    return yoga_series
+
+def cheer_series():
+    cheer_series = ["Marching", "Drums", "On your side", "Dancing","Little Cheerieding", "Cheering", "Trophy Angel"]
+    return cheer_series
+
+def museum_series():
+    museum_series = ["The Source", "Fuzin & Raijin", "Bacchus", "Vela'zquez","Dali", "Pearl Earring", "King Tut"]
+    return museum_series
+
+def work_series():
+    work_series = ["Approving", "Researching", "Presenting", "Good Idea", "On the Road", "Little Group Think", "Lucky Cat"]
+    return work_series
+
+def dressing_series():
+    dressing_series = ["Underpants", "Struggling", "Loose Pants", "Putting on Socks", "Sweater", "Tight Pants", "Costume Bunny"]
+    return dressing_series
+
+def exercising_series():
+    exercising_series = ["Doing crunches", "Aerobics", "Little Balance", "Dumbbell","Hoop","Stretch", "Flexing"]
+    return exercising_series
+
+def moving_series():
+    moving_series = ["Carrying Ladder", "Balancing Boxes", "Decorating", "Little Teamwork", "Green Thumb","Falling Down","Teddy"]
+    return moving_series
+
+def hippers_series():
+    hippers_series = ["On his Smartphone", "Trying to Climb", "Looking Out", "Sticking", "Dozing", "Upside Down", "Headphones"]
+    return hippers_series
+
+def sunday_series():
+    sunday_series = ["Blowing Bubbles", "Paper Airplane", "Sunbathing", "Sing-Along","Skateboarding","Gardening", "Dog Walking"]
+    return sunday_series
+
